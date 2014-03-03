73d8111a18d015af4053969db95a8ee8ecf66351
Roni.Chen <roni.chen@comcast.net>
2014-03-03 10:59:27 -0700

    final commit

--- spec.js
+++ spec.js
@@ -1,3 +1,12 @@
 console.log('TODO: Add tests to spec.js');
 
 //assert(somethingThatShouldEqualTen, 10);
+
+assert(isEven(10), true);
+assert(isEven(244), true);
+assert(isEven(0), true);
+assert(isEven(-4), true);
+assert(isEven(3), false);
+assert(isEven(25), false);
+assert(isEven(-1), false);
+assert(isEven(-53), false);
